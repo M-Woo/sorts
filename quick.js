9e8a6d71d56e27f851ce74da7f7328bcc6b3b04c
m-woo <mark.chi.woo@gmail.com>
2017-05-28 20:52:47 -0700

    quick sort exp

--- quick.js
+++ quick.js
@@ -1,3 +1,12 @@
+//1 - Find a “pivot” item in the array. This item is the basis for comparison for a single round.
+//2 - Start a pointer (the left pointer) at the first item in the array.
+//3 - Start a pointer (the right pointer) at the last item in the array.
+//4 - While the value at the left pointer in the array is less than the pivot value, move the left pointer to the right (add 1). Continue until the value at the left pointer is greater than or equal to the pivot value.
+//5 - While the value at the right pointer in the array is greater than the pivot value, move the right pointer to the left (subtract 1). Continue until the value at the right pointer is less than or equal to the pivot value.
+//6 - If the left pointer is less than or equal to the right pointer, then swap the values at these locations in the array.
+//7 - Move the left pointer to the right by one and the right pointer to the left by one.
+//8 - If the left pointer and right pointer don’t meet, go to step 1.
+
 function quickSort(arr, left, right){
    var len = arr.length, 
    pivot,
@@ -37,3 +46,4 @@ function swap(arr, i, j){
    arr[i] = arr[j];
    arr[j] = temp;
 }
+
