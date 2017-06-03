5f43cd7fb353d74e6656a33f7e02f906d1beadb2
m-woo <mark.chi.woo@gmail.com>
2017-06-02 20:32:52 -0700

    additional heap sort

--- heap.js
+++ heap.js
@@ -1,3 +1,7 @@
+
+
+
+
 function heapSort(arr){
   var len = arr.length,
       end = len-1;
@@ -52,4 +56,57 @@ function swap(arr, i, j){
   arr[i] = arr[j];
   arr[j] = temp;
 }
+
+
+//====================================================
+
+
+var a = [ 9, 10, 2, 1, 5, 4, 3, 6, 8, 7, 13 ];
+
+function swap(a, i, j) {
+    var tmp = a[i];
+    a[i] = a[j];
+    a[j] = tmp;
+}
+
+function max_heapify(a, i, length) {
+    while (true) {
+        var left = i*2 + 1;
+        var right = i*2 + 2;
+        var largest = i;
+
+        if (left < length && a[left] > a[largest]) {
+            largest = left;
+        }
+
+        if (right < length && a[right] > a[largest]) {
+            largest = right;
+        }
+
+        if (i == largest) {
+            break;
+        }
+
+        swap(a, i, largest);
+        i = largest;
+    }
+}
+
+function heapify(a, length) {
+    for (var i = Math.floor(length/2); i >= 0; i--) {
+        max_heapify(a, i, length);
+    }
+}
+
+function heapsort(a) {
+    heapify(a, a.length);
+
+    for (var i = a.length - 1; i > 0; i--) {
+        swap(a, i, 0);
+
+        max_heapify(a, 0, i-1);
+    }
+}
+
+heapsort(a);
    
